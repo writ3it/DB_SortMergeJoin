90bc96ae957ba501e4016c450e6463c5f4ef2363
writ3it <42292507+writ3it@users.noreply.github.com>
2018-12-04 20:12:51 +0100

    more advanced test

--- test/DBObject/testBFile.py
+++ test/DBObject/testBFile.py
@@ -4,6 +4,7 @@ from DBObject.Buffer import Buffer
 from DBObject.MemBlock import MemBlock
 
 class FileTestCase(unittest.TestCase):
+
     def test_reading(self):
         size = 1000
         bSize = 10
@@ -12,9 +13,10 @@ class FileTestCase(unittest.TestCase):
         expected = 1000 / 10
         i = 0
         while not file.eof():
-            i = i + 1
             block = file.read()
             self.assertIsInstance(block, MemBlock)
+            self.assertEqual(i*bSize, block.GetPosition(),msg="Niewłaściwa lokalizacja bloków")
+            i = i + 1
         self.assertEqual(expected, i, msg="Odczytano niewłasciwą liczbę bloków")
 
 if __name__ == '__main__':
