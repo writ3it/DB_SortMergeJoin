7613ee3832b2ec5b2c10a21f73ba56753e60f212
writ3it <42292507+writ3it@users.noreply.github.com>
2019-02-21 23:15:06 +0100

    looks like works but very slow

--- test/tableTest.py
+++ test/tableTest.py
@@ -50,3 +50,29 @@ class NestedLoopTest(unittest.TestCase):
 
         self.assertEqual(no_blocks * block_size, i, "Total outside")
         self.assertEqual(no_blocks * block_size, j, "Total inside")
+
+
+    def test_with_generators(self):
+        no_blocks = 100
+        block_size = 5
+        key_size = 1
+        buffer = Buffer(M=100)
+        fR = DataFile(file_size_in_blocks=no_blocks, key_size=key_size, block_size=block_size)
+        fS = DataFile(file_size_in_blocks=no_blocks, key_size=key_size, block_size=block_size)
+        R = Table(fR, buffer.GetMemorySpace(1))
+        S = Table(fS, buffer.GetMemorySpace(1))
+
+        i = 0
+        for row in R.GetRows():
+            j = 0
+            for nrow in S.GetRows():
+                self.assertEqual(j, nrow[0], msg="Nested step Error")
+                j += key_size
+                self.assertGreaterEqual(no_blocks * block_size, j, msg="Nested limit")
+            S.Reset()
+            self.assertEqual(i, row[0], msg="Step Error")
+            i += key_size
+            self.assertGreaterEqual(no_blocks * block_size, i, msg="Limit")
+
+        self.assertEqual(no_blocks * block_size, i, "Total outside")
+        self.assertEqual(no_blocks * block_size, j, "Total inside")
